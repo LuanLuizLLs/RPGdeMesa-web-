66030f55d03c7e4427db7b04bc1f537e51707b99
Luan Silva <37859294+LuanLuizLLs@users.noreply.github.com>
2022-08-06 21:54:01 -0300

    Refactored login

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -41,9 +41,9 @@ function Login() {
   const [view, setView] = useState(INITIAL.VIEW)
   const [values, setValues] = useState(INITIAL.VALUES)
 
-  const handleClick = {
-    openLogin: () => {
-      setView('login')
+  const handle = {
+    alterView: (view) => {
+      setView(view)
       setValues(INITIAL.VALUES)
     },
     submitLogin: () => {
@@ -81,10 +81,6 @@ function Login() {
           setLoading({})
         })
     },
-    openRegister: () => {
-      setView('register')
-      setValues(INITIAL.VALUES)
-    },
     submitRegister: () => {
       if (isNull(values)) {
         return setMessage({
@@ -114,13 +110,6 @@ function Login() {
           setView(INITIAL.VIEW)
         })
     },
-    openRecover: () => {
-      setView('recover')
-      setValues({
-        ...INITIAL.VALUES,
-        user: values.user,
-      })
-    },
     submitRecover: () => {
       if (isNull(values)) {
         return setMessage({
@@ -138,8 +127,8 @@ function Login() {
 
       API.patch(`users/update`, values)
         .then(({ data }) => {
-          setMessage(data.message)
           setView('login')
+          setMessage(data.message)
         })
         .catch(() => {
           setMessage({
@@ -150,175 +139,171 @@ function Login() {
     },
   }
 
-  const Content = (content) => {
-    return ({
-      login: (
-        <>
-          <Image
-            src={Logo}
-            alt="logo"
-            maxWidth={100}
-            maxHeight={100}
-            margin="10px auto"
-          />
-          <Title type="h1" color="primary" textAlign="center">
-            Login
-          </Title>
-          <Input
-            name="user"
-            label="Usuário"
-            placeholder="Digite seu usuário"
-            stateValue={[values, setValues]}
-            onEnter={handleClick.submitLogin}
-          />
-          <Input
-            name="password"
-            label="Senha"
-            type="password"
-            placeholder="Digite sua senha"
-            stateValue={[values, setValues]}
-            onEnter={handleClick.submitLogin}
-          />
-          <Grid type="container" padding={[20, 0]}>
-            <Grid type="row">
-              <Grid type="column" size={6} margin={[0, 10]}>
-                <Button type="filled" color="secondary" onClick={handleClick.openRegister}>
-                  Cadastrar-se
-                </Button>
-              </Grid>
-              <Grid type="column" size={6} margin={[0, 10]}>
-                <Button type="filled" onClick={handleClick.submitLogin}>
-                  Logar
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Box width="fit-content" margin="0 auto">
-            <Link onClick={handleClick.openRecover}>
-              Esqueceu sua senha?
-            </Link>
-          </Box>
-        </>
-      ),
-      register: (
-        <>
-          <Image
-            src={Logo}
-            alt="logo"
-            maxWidth={100}
-            maxHeight={100}
-            margin="10px auto"
-          />
-          <Title type="h1" color="primary" textAlign="center">
-            Cadastre-se
-          </Title>
-          <Title type="h6" color="primary" textAlign="center">
-            Preencha os dados a seguir:
-          </Title>
-          <Input
-            name="user"
-            label="Usuário"
-            placeholder="Seu novo usuário"
-            stateValue={[values, setValues]}
-            onEnter={handleClick.submitRegister}
-          />
-          <Input
-            name="password"
-            label="Senha"
-            type="password"
-            placeholder="Digite sua senha"
-            stateValue={[values, setValues]}
-            onEnter={handleClick.submitRegister}
-            validate={comparativePassword(values.password, values.new_password)}
-          />
-          <Input
-            name="new_password"
-            label="Confirmar senha"
-            type="password"
-            placeholder="Confirme sua senha"
-            stateValue={[values, setValues]}
-            onEnter={handleClick.submitRegister}
-            validate={comparativePassword(values.password, values.new_password)}
-          />
-          <Grid type="container" padding={[20, 0]}>
-            <Grid type="row">
-              <Grid type="column" size={6} margin={[0, 10]}>
-                <Button type="filled" color="secondary" onClick={handleClick.openLogin}>
-                  Voltar
-                </Button>
-              </Grid>
-              <Grid type="column" size={6} margin={[0, 10]}>
-                <Button type="filled" onClick={handleClick.submitRegister}>
-                  Cadastrar
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </>
-      ),
-      recover: (
-        <>
-          <Image
-            src={Logo}
-            alt="logo"
-            maxWidth={100}
-            maxHeight={100}
-            margin="10px auto"
-          />
-          <Title type="h1" color="primary" textAlign="center">
-            Recuperar
-          </Title>
-          <Title type="h6" color="primary" textAlign="center">
-            Altere sua senha:
-          </Title>
-          <Input
-            name="user"
-            label="Usuário"
-            placeholder="Digite seu usuário"
-            stateValue={[values, setValues]}
-            onEnter={handleClick.submitRecover}
-          />
-          <Input
-            name="password"
-            label="Nova senha"
-            type="password"
-            placeholder="Digite sua senha"
-            stateValue={[values, setValues]}
-            onEnter={handleClick.submitRecover}
-            validate={comparativePassword(values.password, values.new_password)}
-          />
-          <Input
-            name="new_password"
-            label="Confirmar senha"
-            type="password"
-            placeholder="Digite sua senha novamente"
-            stateValue={[values, setValues]}
-            onEnter={handleClick.submitRecover}
-            validate={comparativePassword(values.password, values.new_password)}
-          />
-          <Grid type="container" padding={[20, 0]}>
-            <Grid type="row">
-              <Grid type="column" size={6} margin={[0, 10]}>
-                <Button type="filled" color="secondary" onClick={handleClick.openLogin}>
-                  Cancelar
-                </Button>
-              </Grid>
-              <Grid type="column" size={6} margin={[0, 10]}>
-                <Button type="filled" onClick={handleClick.submitRecover}>
-                  Salvar
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </>
-      ),
-    })[content] || 'Conteúdo não encontrado...'
-  }
-
   return (
     <Box flex="auto" maxWidth="90vw">
       <Card maxWidth="350px">
-        {Content(view)}
+        {({
+          login: (
+            <>
+              <Image
+                src={Logo}
+                alt="logo"
+                maxWidth={100}
+                maxHeight={100}
+                margin="10px auto"
+              />
+              <Title type="h1" color="primary" textAlign="center">
+                Login
+              </Title>
+              <Input
+                name="user"
+                label="Usuário"
+                placeholder="Digite seu usuário"
+                stateValue={[values, setValues]}
+                onEnter={handle.submitLogin}
+              />
+              <Input
+                name="password"
+                label="Senha"
+                type="password"
+                placeholder="Digite sua senha"
+                stateValue={[values, setValues]}
+                onEnter={handle.submitLogin}
+              />
+              <Grid type="container" padding={[20, 0]}>
+                <Grid type="row">
+                  <Grid type="column" size={6} margin={[0, 10]}>
+                    <Button type="filled" color="secondary" onClick={() => handle.alterView('register')}>
+                      Cadastrar-se
+                    </Button>
+                  </Grid>
+                  <Grid type="column" size={6} margin={[0, 10]}>
+                    <Button type="filled" onClick={handle.submitLogin}>
+                      Logar
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Box width="fit-content" margin="0 auto">
+                <Link onClick={() => handle.alterView('recover')}>
+                  Esqueceu sua senha?
+                </Link>
+              </Box>
+            </>
+          ),
+          register: (
+            <>
+              <Image
+                src={Logo}
+                alt="logo"
+                maxWidth={100}
+                maxHeight={100}
+                margin="10px auto"
+              />
+              <Title type="h1" color="primary" textAlign="center">
+                Cadastre-se
+              </Title>
+              <Title type="h6" color="primary" textAlign="center">
+                Preencha os dados a seguir:
+              </Title>
+              <Input
+                name="user"
+                label="Usuário"
+                placeholder="Seu novo usuário"
+                stateValue={[values, setValues]}
+                onEnter={handle.submitRegister}
+              />
+              <Input
+                name="password"
+                label="Senha"
+                type="password"
+                placeholder="Digite sua senha"
+                stateValue={[values, setValues]}
+                onEnter={handle.submitRegister}
+                validate={comparativePassword(values.password, values.new_password)}
+              />
+              <Input
+                name="new_password"
+                label="Confirmar senha"
+                type="password"
+                placeholder="Confirme sua senha"
+                stateValue={[values, setValues]}
+                onEnter={handle.submitRegister}
+                validate={comparativePassword(values.password, values.new_password)}
+              />
+              <Grid type="container" padding={[20, 0]}>
+                <Grid type="row">
+                  <Grid type="column" size={6} margin={[0, 10]}>
+                    <Button type="filled" color="secondary" onClick={() => handle.alterView(INITIAL.VIEW)}>
+                      Voltar
+                    </Button>
+                  </Grid>
+                  <Grid type="column" size={6} margin={[0, 10]}>
+                    <Button type="filled" onClick={handle.submitRegister}>
+                      Cadastrar
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </>
+          ),
+          recover: (
+            <>
+              <Image
+                src={Logo}
+                alt="logo"
+                maxWidth={100}
+                maxHeight={100}
+                margin="10px auto"
+              />
+              <Title type="h1" color="primary" textAlign="center">
+                Recuperar
+              </Title>
+              <Title type="h6" color="primary" textAlign="center">
+                Altere sua senha:
+              </Title>
+              <Input
+                name="user"
+                label="Usuário"
+                placeholder="Digite seu usuário"
+                stateValue={[values, setValues]}
+                onEnter={handle.submitRecover}
+              />
+              <Input
+                name="password"
+                label="Nova senha"
+                type="password"
+                placeholder="Digite sua senha"
+                stateValue={[values, setValues]}
+                onEnter={handle.submitRecover}
+                validate={comparativePassword(values.password, values.new_password)}
+              />
+              <Input
+                name="new_password"
+                label="Confirmar senha"
+                type="password"
+                placeholder="Digite sua senha novamente"
+                stateValue={[values, setValues]}
+                onEnter={handle.submitRecover}
+                validate={comparativePassword(values.password, values.new_password)}
+              />
+              <Grid type="container" padding={[20, 0]}>
+                <Grid type="row">
+                  <Grid type="column" size={6} margin={[0, 10]}>
+                    <Button type="filled" color="secondary" onClick={() => handle.alterView(INITIAL.VIEW)}>
+                      Cancelar
+                    </Button>
+                  </Grid>
+                  <Grid type="column" size={6} margin={[0, 10]}>
+                    <Button type="filled" onClick={handle.submitRecover}>
+                      Salvar
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </>
+          ),
+        })[view]}
       </Card>
     </Box>
   )
