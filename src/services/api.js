e7b2270bddab4a4c7b463303362f69c580ab11fe
Luan Silva <37859294+LuanLuizLLs@users.noreply.github.com>
2022-09-07 09:28:53 -0300

    Fix delete in API

--- src/services/api.js
+++ src/services/api.js
@@ -11,7 +11,7 @@ export const requestAPI = (prefix = '', data = {}) => {
     create: (callback) => API.post(`${prefix}/create`, data).then(callback),
     read: (callback) => API.get(`${prefix}/read`, { params: data }).then(callback),
     update: (callback) => API.patch(`${prefix}/update`, data).then(callback),
-    delete: (callback) => API.delete(`${prefix}/delete`, data).then(callback),
+    delete: (callback) => API.delete(`${prefix}/delete/${data}`).then(callback),
   }
 }
 
