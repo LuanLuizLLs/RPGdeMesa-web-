147e9e3a3f22d4f14ab184e7bc421e803772841b
Luan Silva <37859294+LuanLuizLLs@users.noreply.github.com>
2022-09-26 22:59:01 -0300

    Interaction coding init

--- src/views/Master/containers/Interaction.js
+++ src/views/Master/containers/Interaction.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, List, Select } from '../../../components'
+
+const INITIAL = {
+  VALUES: {
+    filter: 'Humanoides',
+  },
+  INTERACTION: {
+    columns: {
+      id: 'ID',
+      name: 'Interação',
+      description: 'Descrição',
+      level: 'Nível',
+    },
+    rows: [],
+  } 
+}
 
 function Interaction() {
+
+  const [values, setValues] = useState(INITIAL.VALUES)
+  const [interaction, ] = useState(INITIAL.INTERACTION)
+
   return (
     <>
-      INTERAÇÃO
+      <List
+        height={200}
+        onClick={(row) => console.log(row)}
+        {...interaction}
+      />
+      <Box display="flex" justifyContent="space-between" margin={10}>
+        <Select
+          name="filter"
+          options={[
+            'Humanoides',
+            'Animais',
+            'Monstros'
+          ]}
+          stateValue={[values, setValues]}
+        />
+        <Button type="filled" onClick={() => {}}>
+          Adicionar
+        </Button>
+      </Box>
     </>
   )
 }
