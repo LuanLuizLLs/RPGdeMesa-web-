71666d7ee898297ae370edc609d2c707c8b62ea8
Luan Silva <37859294+LuanLuizLLs@users.noreply.github.com>
2023-06-03 20:04:14 -0300

    Refactor API

--- src/services/api.js
+++ src/services/api.js
@@ -1,29 +1,15 @@
 import axios from 'axios'
 
-/** Service API */
-const base = axios.create({
+const api = axios.create({
 	baseURL: `http://${window.location.hostname}:8080`,
 })
 
-/**
- * Request to API
- * @param {String} prefix route name
- * @param {Object} params parameters for sending
- * @returns
- */
 const API = (prefix = '', params = {}) => {
-	const routes = {
-		create: `${prefix}/create`,
-		read: `${prefix}/read`,
-		update: `${prefix}/update`,
-		delete: `${prefix}/delete`,
-	}
-
 	return {
-		create: (callback) => base.post(routes.create, params).then(callback),
-		read: (callback) => base.get(routes.read, { params }).then(callback),
-		update: (callback) => base.patch(routes.update, params).then(callback),
-		delete: (callback) => base.delete(routes.delete, { params }).then(callback),
+		create: (callback) => api.post(`${prefix}/create`, params).then(callback),
+		read: (callback) => api.get(`${prefix}/read`, { params }).then(callback),
+		update: (callback) => api.patch(`${prefix}/update`, params).then(callback),
+		delete: (callback) => api.delete(`${prefix}/delete`, { params }).then(callback),
 	}
 }
 
