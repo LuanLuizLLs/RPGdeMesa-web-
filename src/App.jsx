519138a09daeff9d1453f6277c498be2897574ce
LuanLuizLLs <luanluiz.dev@gmail.com>
2024-05-29 21:37:12 -0300

    refactor: app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import Routes from './routes'
-import Message from './layouts/Message'
-import Loading from './layouts/Loading'
-import GlobalContextProvider from './context'
+import Routes from 'routes'
+import Message from 'layouts/Message'
+import Loading from 'layouts/Loading'
+import GlobalContextProvider from 'context'
 
 function App() {
 	const setNavigate = useNavigate()
