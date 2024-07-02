a1e776d03aea54196ccf2cdc757fa5b236f004e8
LuanLuizLLs <luanluiz.dev@gmail.com>
2024-07-01 21:26:33 -0300

    chore: add typescript in project

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+
+interface IBox extends React.CSSProperties {
+  children: React.ReactNode
+}
 
 export const Box = ({
 	children, ...style
-}) => {
+}: IBox) => {
 	return (
 		<div style={style}>
 			{children}
 		</div>
 	)
-}
-
-Box.propTypes = {
-	children: PropTypes.any,
-	style: PropTypes.object,
 }
