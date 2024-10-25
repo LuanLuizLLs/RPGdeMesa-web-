40132b8a3b67936d511acadbc734d7ff462f74b2
LuanLuizLLs <luanluiz.dev@gmail.com>
2024-10-25 17:29:07 -0300

    feat: add try catch

--- src/pages/Home/components/Campaigns/utils/functions/index.js
+++ src/pages/Home/components/Campaigns/utils/functions/index.js
@@ -2,9 +2,14 @@ import { CONDITIONS } from 'utils/constants'
 import { numberRandow } from 'utils/functions'
 
 export const campaignAttributes = (period = '', climate = '') => {
-	const [min, max] = CONDITIONS[period][climate]
-	return {
-		ground: numberRandow(min, max),
-		resources: numberRandow(min, max),
+	try {
+		const [min, max] = CONDITIONS[period][climate]
+  
+		return {
+			ground: numberRandow(min, max),
+			resources: numberRandow(min, max),
+		}
+	} catch {
+		return {}
 	}
 }
--- src/pages/Home/components/Characters/utils/functions/index.js
+++ src/pages/Home/components/Characters/utils/functions/index.js
@@ -2,24 +2,28 @@ import { RACE, CASTE } from 'utils/constants'
 import { numberRandow } from 'utils/functions'
 
 export const characterAttributes = (race = '', caste = '') => {
-	const {
-		strength: [strengthMin, strengthMax],
-		dexterity: [dexterityMin, dexterityMax],
-		constitution: [constitutionMin, constitutionMax],
-		intelligence: [intelligenceMin, intelligenceMax],
-		wisdom: [wisdomMin, wisdomMax],
-		charisma: [charismaMin, charismaMax],
-	} = {
-		...RACE[race],
-		...CASTE[caste],
-	}
-  
-	return {
-		strength: numberRandow(strengthMin, strengthMax),
-		dexterity: numberRandow(dexterityMin, dexterityMax),
-		constitution: numberRandow(constitutionMin, constitutionMax),
-		intelligence: numberRandow(intelligenceMin, intelligenceMax),
-		wisdom: numberRandow(wisdomMin, wisdomMax),
-		charisma: numberRandow(charismaMin, charismaMax),
+	try {
+		const {
+			strength: [strengthMin, strengthMax],
+			dexterity: [dexterityMin, dexterityMax],
+			constitution: [constitutionMin, constitutionMax],
+			intelligence: [intelligenceMin, intelligenceMax],
+			wisdom: [wisdomMin, wisdomMax],
+			charisma: [charismaMin, charismaMax],
+		} = {
+			...RACE[race],
+			...CASTE[caste],
+		}
+    
+		return {
+			strength: numberRandow(strengthMin, strengthMax),
+			dexterity: numberRandow(dexterityMin, dexterityMax),
+			constitution: numberRandow(constitutionMin, constitutionMax),
+			intelligence: numberRandow(intelligenceMin, intelligenceMax),
+			wisdom: numberRandow(wisdomMin, wisdomMax),
+			charisma: numberRandow(charismaMin, charismaMax),
+		}
+	} catch {
+		return {}
 	}
 }
