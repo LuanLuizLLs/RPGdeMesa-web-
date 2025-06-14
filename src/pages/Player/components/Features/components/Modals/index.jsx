f557bfcc51b1b2fd76337e7a9ae7aa5193716368
LuanLuizLLs <luanluiz.dev@gmail.com>
2025-06-14 20:20:07 -0300

    refactor: adicionar campos ao editar as características

--- src/pages/Player/components/Features/components/Modals/index.jsx
+++ src/pages/Player/components/Features/components/Modals/index.jsx
@@ -160,6 +160,76 @@ export function Modals({ handle, stateModal, stateValues }) {
 					placeholder="Descrição"
 					stateValue={stateValues}
 				/>
+				<Grid type="row" padding={[5, 0]}>
+					<Grid type="column" margin={[0, 5]}>
+						<Input
+							max={1}
+							min={-1}
+							start="💪"
+							label="FOR"
+							type="number"
+							name="strength"
+							stateValue={stateValues}
+						/>
+					</Grid>
+					<Grid type="column" margin={[0, 5]}>
+						<Input
+							max={1}
+							min={-1}
+							start="👋"
+							label="DES"
+							type="number"
+							name="dexterity"
+							stateValue={stateValues}
+						/>
+					</Grid>
+					<Grid type="column" margin={[0, 5]}>
+						<Input
+							max={1}
+							min={-1}
+							start="✊"
+							label="CON"
+							type="number"
+							name="constitution"
+							stateValue={stateValues}
+						/>
+					</Grid>
+				</Grid>
+				<Grid type="row">
+					<Grid type="column" margin={[0, 5]}>
+						<Input
+							max={1}
+							min={-1}
+							start="📙"
+							label="INT"
+							type="number"
+							name="intelligence"
+							stateValue={stateValues}
+						/>
+					</Grid>
+					<Grid type="column" margin={[0, 5]}>
+						<Input
+							max={1}
+							min={-1}
+							start="🙌"
+							label="SAB"
+							type="number"
+							name="wisdom"
+							stateValue={stateValues}
+						/>
+					</Grid>
+					<Grid type="column" margin={[0, 5]}>
+						<Input
+							max={1}
+							min={-1}
+							start="🤝"
+							label="CAR"
+							type="number"
+							name="charisma"
+							stateValue={stateValues}
+						/>
+					</Grid>
+				</Grid>
 				<Box display="flex" justifyContent="flex-end" marginTop={10}>
 					<Button type="filled" color="secondary" padding={10} onClick={handle.resetFeature}>
             Cancelar
